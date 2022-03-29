daa9afdf77636de5c541b606f1e1a22b7c784905
Christopher Kim <chris.kim1990@gmail.com>
2022-03-29 10:39:43 -0400

    review

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+const namesArr = ["Ada", "Brendan", "Ali"]
+
+function writeCards( namesArr, event ){
+    let thankYouCards = []
+    for ( let i = 0; i < namesArr.length; i++ ){
+        thankYouCards.push( `Thank you, ${namesArr[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankYouCards
+}
+// console.log(writeCards(namesArr,"birthday"))
+
+function countDown( startingNum ){
+    while ( startingNum > 0 ) {
+        console.log( startingNum );
+        startingNum -= 1;
+    }
+    console.log( startingNum );
+}
+
+// console.log(countDown(10))
+
+
